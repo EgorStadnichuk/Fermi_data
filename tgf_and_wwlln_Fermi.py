8b7d9633beeadb6adc3862d0de6dfb3c8de56fea
EgorStadnichuk <egrstadnichuk@gmail.com>
2020-08-05 09:54:55 +0300

    Removed function move_spherical_coordinate_center_to_point()
    The function detector_cloud_angle() now calculates the angle straightforwardly.
    
    The function angle_two_points() was used for tests.
    
    Tests showed the correct work of both functions on simple examples.

--- tgf_and_wwlln_Fermi.py
+++ tgf_and_wwlln_Fermi.py
@@ -35,23 +35,29 @@ def angle_two_points(lon1, lat1, r1, lon2, lat2, r2):
     return angle
 
 
-# __________changes coordinates of a spherical vector vector to spherical coordinates with a center in vector new_center
-# __________primary coordinates: Earth coordinates
-# ----------vector = (lon, lat, r)
-def move_spherical_coordinate_center_to_point(vector, new_center):
-    descartes_vector = np.array([vector[2] * math.cos(vector[1]) * math.cos(vector[0]), vector[2] * math.cos(vector[1]) * math.sin(vector[0]), vector[2] * math.sin(vector[1])])
-    descartes_new_center = np.array([new_center[2] * math.cos(new_center[1]) * math.cos(new_center[0]), new_center[2] * math.cos(new_center[1]) * math.sin(new_center[0]), new_center[2] * math.sin(new_center[1])])
-    new_r = np.sqrt(np.square(descartes_vector[0] - descartes_new_center[0]) + np.square(descartes_vector[1] - descartes_new_center[1]) + np.square(descartes_vector[2] - descartes_new_center[2]))
-    new_r_projected_xy = np.sqrt(np.square(descartes_vector[0] - descartes_new_center[0]) + np.square(descartes_vector[1] - descartes_new_center[1]))
-    return np.arccos((descartes_vector[0] - descartes_new_center[0]) / new_r_projected_xy), np.arcsin((descartes_vector[2] - descartes_new_center[2]) / new_r), new_r
-
-
+# _________a function to find the angle between the TGF source (correlated lightning source) and the detector
 def detector_cloud_angle(lon1, lat1, r1, lon2, lat2, r2):
-    detector_spherical_vector = np.array([lon1, lat1, r1])
-    cloud_spherical_vector = np.array([lon2, lat2, r2])
-    detector = move_spherical_coordinate_center_to_point(detector_spherical_vector, cloud_spherical_vector)
-    cloud = move_spherical_coordinate_center_to_point(cloud_spherical_vector + np.array([0, 0, 1.0]), cloud_spherical_vector)
-    return angle_two_points(detector[0], detector[1], detector[2], cloud[0], cloud[1], cloud[2])
+    # conversion from a spherical coordinate system to a Cartesian one
+    lat1_rad = lat1 * math.pi / 180
+    lon1_rad = lon1 * math.pi / 180
+    lat2_rad = lat2 * math.pi / 180
+    lon2_rad = lon2 * math.pi / 180
+    descartes_1 = np.array([r1 * math.cos(lat1_rad) * math.cos(lon1_rad), r1 * math.cos(lat1_rad) * math.sin(lon1_rad),
+                            r1 * math.sin(lat1_rad)])
+    descartes_2 = np.array([r2 * math.cos(lat2_rad) * math.cos(lon2_rad), r2 * math.cos(lat2_rad) * math.sin(lon2_rad),
+                            r2 * math.sin(lat2_rad)])
+    # calculation of the vector between the cloud and the detector
+    vector_between_points = descartes_1 - descartes_2
+    # calculation of the vector normal to the cloud surface in the Cartesian system. In spherical coordinates it has the
+    # same lon and alt, but r = r2 + some small number
+    normal_to_vector_2 = np.array(
+        [(r2 + 1) * math.cos(lat2_rad) * math.cos(lon2_rad), (r2 + 1) * math.cos(lat2_rad) * math.sin(lon2_rad),
+         (r2 + 1) * math.sin(lat2_rad)])
+    # classical way to find an angle between two vectors in the Cartesian system
+    angle = 180 / math.pi * math.acos(scalar_multiplication(vector_between_points, normal_to_vector_2) / np.sqrt(
+        scalar_multiplication(vector_between_points, vector_between_points) * scalar_multiplication(normal_to_vector_2,
+                                                                                                    normal_to_vector_2)))
+    return angle
 
 
 def distance_two_points(lon1, lat1, r1, lon2, lat2, r2):
@@ -106,107 +112,120 @@ def create_datetime_array_for_Fermi(data):
 
 
 def main():
-    time_difference = datetime.timedelta(microseconds=500)
 
-    # ==============================================================================
-    # data = pandas.read_csv('gbm_tgf_catalog_2016_f.csv', index_col=False, header=None)
-    # f_TGF_info = open('Fermi_TGFs_distances_and_time_diff' + '.csv','w')
-    # f_TGF_with_lightning = open('Fermi_TGFs_2016_with_lightning_' + str(time_difference.microseconds) + 'mcs.csv','w')
-    # f_TGF_no_lightning = open('Fermi_TGFs_2016_no_lightning_'+ str(time_difference.microseconds) + 'mcs.csv','w')
-    # ==============================================================================
+    lon1 = 0.0
+    lat1 = 0.0
+    r1 = 6500.0
 
-    data = pandas.read_csv('gbm_tgf_catalog_2016.csv', index_col=False, header=None)
-    f_TGF_info = open('Fermi_TGFs_distances_and_time_diff' + '.csv', 'w')
-    f_TGF_with_lightning = open('Fermi_TGFs_2016_with_lightning_' + str(time_difference.microseconds) + 'mcs.csv', 'w')
-    f_TGF_no_lightning = open('Fermi_TGFs_2016_no_lightning_' + str(time_difference.microseconds) + 'mcs.csv', 'w')
-    # Files where will be TGFs altitude, number of counts, duration
+    lon2 = 0.0
+    lat2 = 1.0
+    r2 = 6400.0
 
-    lon_tgf = []
-    lat_tgf = []
-    alt_tgf = []
-    counts_NaI = []
-    duration_tgf = []
-    Earth_radius = 6371.0
+    print(angle_two_points(lon1, lat1, r1, lon2, lat2, r2))
+    print(detector_cloud_angle(lon1, lat1, r1, lon2, lat2, r2))
+    # print(angle_two_points(lon1, lat1, r1, lon2, lat2, r2))
 
-    # r_l = Earth_radius + 10.0
-    r_l = Earth_radius
-    speed_of_light = 299792.458  # km per s
-
-    # let's initialize arrays with lat, lon and datetime values for all the TGFs
-    for j_tgf in range(0, len(data)):
-        lon_tgf.append((data.iloc[j_tgf, 10]))
-        lat_tgf.append((data.iloc[j_tgf, 11]))
-        alt_tgf.append((data.iloc[j_tgf, 12]))
-        counts_NaI.append((data.iloc[j_tgf, 5]))
-        duration_tgf.append((data.iloc[j_tgf, 8]))
-
-    time_tgf = create_datetime_array_for_Fermi(data)
-    quantity_of_columns = 1
-
-    for j_tgf in range(0, len(data)):
-        f_TGF_info.write(str(time_tgf[j_tgf]) + '   ')
-
-        date_and_time = data.iloc[j_tgf, 6][1:5] + data.iloc[j_tgf, 6][6:8] + data.iloc[j_tgf, 6][9:11] + '_'
-        date_and_time += data.iloc[j_tgf, 7][1:3] + data.iloc[j_tgf, 7][4:6] + data.iloc[j_tgf, 7][7:9]
-
-    #     data_wwlln = pandas.read_csv('WWLLN_csv/F' + date_and_time + '.csv', index_col=False, header=None)
-    #     time_wwlln = create_datetime_array_from_file(data_wwlln)
+    # time_difference = datetime.timedelta(microseconds=500)
     #
-    #     f_current_lightning = open('dif/F' + date_and_time + '_time_diff' + '.csv', 'w')
-    #     minimal_time_diff = datetime.timedelta(seconds=1)
-    #     minimal_time_distance = 0
-    #     min_time_offset = 0
-    #     min_time_sign = '!'
-    #     sign = '%'
-    #     for j_wwlln in range (0, len(data_wwlln)):
-    #         lat_l = float(data_wwlln.iloc[j_wwlln, 2])
-    #         lon_l = float(data_wwlln.iloc[j_wwlln, 3])
-    # #        time_for_light_transition = datetime.timedelta(0,0,0)
-    #         distance_for_light_transition = distance_two_points(lon_tgf[j_tgf], lat_tgf[j_tgf], alt_tgf[j_tgf] + Earth_radius, lon_l, lat_l, r_l)
-    #         offset = distance_two_points(lon_tgf[j_tgf], lat_tgf[j_tgf], Earth_radius, lon_l, lat_l, Earth_radius)
-    # #        distance_for_light_transition = alt_tgf[j_tgf] - offset
-    #         time_for_light_transition = distance_for_light_transition / speed_of_light  # seconds
-    #         seconds = math.modf(time_for_light_transition)[1]
-    #         microseconds = math.modf(time_for_light_transition)[0] * pow(10, 6)
-    #         time_for_light_transition = datetime.timedelta(seconds=seconds, microseconds=microseconds)
-    #         time_diff = abs(time_tgf[j_tgf] - (time_wwlln[j_wwlln] + time_for_light_transition))
-    #         if time_tgf[j_tgf] > (time_wwlln[j_wwlln] + time_for_light_transition):
-    #             sign = '-'
-    #         else:
-    #             sign = '+'
-    # #        time_diff = abs(time_tgf[j_tgf] - (time_wwlln[j_wwlln]))
-    #         f_current_lightning.write(sign+str(time_diff) + '   ' + str(distance_for_light_transition) + '   WWLLN_lon = ' + str(lon_l) + '   WWLLN_lat = ' + str(lat_l) + '   offset = ' + str(offset) + '\n')
-    #         if time_diff < minimal_time_diff:
-    #             minimal_time_diff = time_diff
-    #             min_time_sign = sign
-    #             minimal_time_distance = distance_for_light_transition
-    #             min_time_offset = offset
-    #             date_stroke = data_wwlln.iloc[j_wwlln, 0]
-    #             time_stroke = data_wwlln.iloc[j_wwlln, 1]
-    #             lat_stroke = data_wwlln.iloc[j_wwlln, 2]
-    #             lon_stroke = data_wwlln.iloc[j_wwlln, 3]
-    #             residual_fit_error = data_wwlln.iloc[j_wwlln, 4]
-    #             number_of_stations = data_wwlln.iloc[j_wwlln, 5]
-    #     f_current_lightning.close()
-    #     f_TGF_info.write('   time_diff = ' + min_time_sign+ str(minimal_time_diff) + '   offset = ' + str(min_time_offset) + '   distance = ' + str(minimal_time_distance) + '\n')
-    #     str_tgf_param = 'date_TGF = ' + str(data.iloc[j_tgf, 6]) + '   time_TGF = ' + str(data.iloc[j_tgf, 7])
-    #     str_tgf_param += '   lat_TGF = ' + str(lat_tgf[j_tgf]) + '   lon_TGF = ' + str(lon_tgf[j_tgf])
-    #     str_tgf_param += '   alt_TGF = ' + str(alt_tgf[j_tgf])+'   counts_TGF = ' + str(counts_NaI[j_tgf])
-    #     str_tgf_param += '   duration = ' + str(duration_tgf[j_tgf])
-    #     str_stroke_param = 'date_stroke = ' + str(date_stroke) + '   time_stroke = ' + str(time_stroke)
-    #     str_stroke_param += '   lat_stroke = ' + str(lat_stroke) + '   lon_stroke = ' + str(lon_stroke)
-    #     str_stroke_param += '   residual_fit_error = ' + str(residual_fit_error) + '   number_of_stations = ' + str(number_of_stations)
-    #     str_comparison_param = 'offset = ' + str(min_time_offset) + '   distance_from_TGF_registration_to_lightning = ' + str(minimal_time_distance)
+    # # ==============================================================================
+    # # data = pandas.read_csv('gbm_tgf_catalog_2016_f.csv', index_col=False, header=None)
+    # # f_TGF_info = open('Fermi_TGFs_distances_and_time_diff' + '.csv','w')
+    # # f_TGF_with_lightning = open('Fermi_TGFs_2016_with_lightning_' + str(time_difference.microseconds) + 'mcs.csv','w')
+    # # f_TGF_no_lightning = open('Fermi_TGFs_2016_no_lightning_'+ str(time_difference.microseconds) + 'mcs.csv','w')
+    # # ==============================================================================
     #
-    #     print('min_time_dif = ', min_time_sign, minimal_time_diff, '\n', 'min_time_offset = ', min_time_offset)
-    #     if minimal_time_diff < time_difference:
-    #         f_TGF_with_lightning.write(str_tgf_param + '\n' + str_stroke_param + '\n' + str_comparison_param + '\n' + '\n')
-    #     else:
-    #         f_TGF_no_lightning.write(str_tgf_param + '\n' + str_stroke_param + '\n' + str_comparison_param + '\n' + '\n')
-
-    f_TGF_info.close()
-    f_TGF_with_lightning.close()
-    f_TGF_no_lightning.close()
+    # data = pandas.read_csv('gbm_tgf_catalog_2016.csv', index_col=False, header=None)
+    # f_TGF_info = open('Fermi_TGFs_distances_and_time_diff' + '.csv', 'w')
+    # f_TGF_with_lightning = open('Fermi_TGFs_2016_with_lightning_' + str(time_difference.microseconds) + 'mcs.csv', 'w')
+    # f_TGF_no_lightning = open('Fermi_TGFs_2016_no_lightning_' + str(time_difference.microseconds) + 'mcs.csv', 'w')
+    # # Files where will be TGFs altitude, number of counts, duration
+    #
+    # lon_tgf = []
+    # lat_tgf = []
+    # alt_tgf = []
+    # counts_NaI = []
+    # duration_tgf = []
+    # Earth_radius = 6371.0
+    #
+    # # r_l = Earth_radius + 10.0
+    # r_l = Earth_radius
+    # speed_of_light = 299792.458  # km per s
+    #
+    # # let's initialize arrays with lat, lon and datetime values for all the TGFs
+    # for j_tgf in range(0, len(data)):
+    #     lon_tgf.append((data.iloc[j_tgf, 10]))
+    #     lat_tgf.append((data.iloc[j_tgf, 11]))
+    #     alt_tgf.append((data.iloc[j_tgf, 12]))
+    #     counts_NaI.append((data.iloc[j_tgf, 5]))
+    #     duration_tgf.append((data.iloc[j_tgf, 8]))
+    #
+    # time_tgf = create_datetime_array_for_Fermi(data)
+    # quantity_of_columns = 1
+    #
+    # for j_tgf in range(0, len(data)):
+    #     f_TGF_info.write(str(time_tgf[j_tgf]) + '   ')
+    #
+    #     date_and_time = data.iloc[j_tgf, 6][1:5] + data.iloc[j_tgf, 6][6:8] + data.iloc[j_tgf, 6][9:11] + '_'
+    #     date_and_time += data.iloc[j_tgf, 7][1:3] + data.iloc[j_tgf, 7][4:6] + data.iloc[j_tgf, 7][7:9]
+    #
+    # #     data_wwlln = pandas.read_csv('WWLLN_csv/F' + date_and_time + '.csv', index_col=False, header=None)
+    # #     time_wwlln = create_datetime_array_from_file(data_wwlln)
+    # #
+    # #     f_current_lightning = open('dif/F' + date_and_time + '_time_diff' + '.csv', 'w')
+    # #     minimal_time_diff = datetime.timedelta(seconds=1)
+    # #     minimal_time_distance = 0
+    # #     min_time_offset = 0
+    # #     min_time_sign = '!'
+    # #     sign = '%'
+    # #     for j_wwlln in range (0, len(data_wwlln)):
+    # #         lat_l = float(data_wwlln.iloc[j_wwlln, 2])
+    # #         lon_l = float(data_wwlln.iloc[j_wwlln, 3])
+    # # #        time_for_light_transition = datetime.timedelta(0,0,0)
+    # #         distance_for_light_transition = distance_two_points(lon_tgf[j_tgf], lat_tgf[j_tgf], alt_tgf[j_tgf] + Earth_radius, lon_l, lat_l, r_l)
+    # #         offset = distance_two_points(lon_tgf[j_tgf], lat_tgf[j_tgf], Earth_radius, lon_l, lat_l, Earth_radius)
+    # # #        distance_for_light_transition = alt_tgf[j_tgf] - offset
+    # #         time_for_light_transition = distance_for_light_transition / speed_of_light  # seconds
+    # #         seconds = math.modf(time_for_light_transition)[1]
+    # #         microseconds = math.modf(time_for_light_transition)[0] * pow(10, 6)
+    # #         time_for_light_transition = datetime.timedelta(seconds=seconds, microseconds=microseconds)
+    # #         time_diff = abs(time_tgf[j_tgf] - (time_wwlln[j_wwlln] + time_for_light_transition))
+    # #         if time_tgf[j_tgf] > (time_wwlln[j_wwlln] + time_for_light_transition):
+    # #             sign = '-'
+    # #         else:
+    # #             sign = '+'
+    # # #        time_diff = abs(time_tgf[j_tgf] - (time_wwlln[j_wwlln]))
+    # #         f_current_lightning.write(sign+str(time_diff) + '   ' + str(distance_for_light_transition) + '   WWLLN_lon = ' + str(lon_l) + '   WWLLN_lat = ' + str(lat_l) + '   offset = ' + str(offset) + '\n')
+    # #         if time_diff < minimal_time_diff:
+    # #             minimal_time_diff = time_diff
+    # #             min_time_sign = sign
+    # #             minimal_time_distance = distance_for_light_transition
+    # #             min_time_offset = offset
+    # #             date_stroke = data_wwlln.iloc[j_wwlln, 0]
+    # #             time_stroke = data_wwlln.iloc[j_wwlln, 1]
+    # #             lat_stroke = data_wwlln.iloc[j_wwlln, 2]
+    # #             lon_stroke = data_wwlln.iloc[j_wwlln, 3]
+    # #             residual_fit_error = data_wwlln.iloc[j_wwlln, 4]
+    # #             number_of_stations = data_wwlln.iloc[j_wwlln, 5]
+    # #     f_current_lightning.close()
+    # #     f_TGF_info.write('   time_diff = ' + min_time_sign+ str(minimal_time_diff) + '   offset = ' + str(min_time_offset) + '   distance = ' + str(minimal_time_distance) + '\n')
+    # #     str_tgf_param = 'date_TGF = ' + str(data.iloc[j_tgf, 6]) + '   time_TGF = ' + str(data.iloc[j_tgf, 7])
+    # #     str_tgf_param += '   lat_TGF = ' + str(lat_tgf[j_tgf]) + '   lon_TGF = ' + str(lon_tgf[j_tgf])
+    # #     str_tgf_param += '   alt_TGF = ' + str(alt_tgf[j_tgf])+'   counts_TGF = ' + str(counts_NaI[j_tgf])
+    # #     str_tgf_param += '   duration = ' + str(duration_tgf[j_tgf])
+    # #     str_stroke_param = 'date_stroke = ' + str(date_stroke) + '   time_stroke = ' + str(time_stroke)
+    # #     str_stroke_param += '   lat_stroke = ' + str(lat_stroke) + '   lon_stroke = ' + str(lon_stroke)
+    # #     str_stroke_param += '   residual_fit_error = ' + str(residual_fit_error) + '   number_of_stations = ' + str(number_of_stations)
+    # #     str_comparison_param = 'offset = ' + str(min_time_offset) + '   distance_from_TGF_registration_to_lightning = ' + str(minimal_time_distance)
+    # #
+    # #     print('min_time_dif = ', min_time_sign, minimal_time_diff, '\n', 'min_time_offset = ', min_time_offset)
+    # #     if minimal_time_diff < time_difference:
+    # #         f_TGF_with_lightning.write(str_tgf_param + '\n' + str_stroke_param + '\n' + str_comparison_param + '\n' + '\n')
+    # #     else:
+    # #         f_TGF_no_lightning.write(str_tgf_param + '\n' + str_stroke_param + '\n' + str_comparison_param + '\n' + '\n')
+    #
+    # f_TGF_info.close()
+    # f_TGF_with_lightning.close()
+    # f_TGF_no_lightning.close()
 
 
 if __name__ == '__main__':
